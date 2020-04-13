aee7e087150745050a3b7ef9c66bccea69c3a19d
Farouq Umar <ufakz99@gmail.com>
2020-04-13 22:52:35 +0100

    Edited estimator.js 13

--- src/index.js
+++ src/index.js
@@ -1,70 +1,70 @@
 const express = require('express');
-const logger = require('morgan')
-const jsonxml = require('jsontoxml')
-const estimator = require('./estimator')
-const fs = require('fs')
-const app = express()
+const logger = require('morgan');
+const jsonxml = require('jsontoxml');
+const estimator = require('./estimator');
+const fs = require('fs');
+const app = express();
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
-app.use(express.static('public'))
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static('public'));
 app.use(logger(":method\t\t:url\t\t:status\t\t:response-time ms",
-  {
-    stream: fs.createWriteStream(`${__dirname}/api.log`,
-      { flags: 'a' })
-  }));
+    {
+        stream: fs.createWriteStream(`${__dirname}/api.log`,
+            { flags: 'a' })
+    }));
 
 const inputData = {
-  region: {
-    name: "Africa",
-    avgAge: 19.7,
-    avgDailyIncomeInUSD: 4,
-    avgDailyIncomePopulation: 0.73
-  },
-  periodType: "days",
-  timeToElapse: 38,
-  reportedCases: 2747,
-  population: 92931687,
-  totalHospitalBeds: 678874
-}
+    region: {
+        name: "Africa",
+        avgAge: 19.7,
+        avgDailyIncomeInUSD: 4,
+        avgDailyIncomePopulation: 0.73
+    },
+    periodType: "days",
+    timeToElapse: 38,
+    reportedCases: 2747,
+    population: 92931687,
+    totalHospitalBeds: 678874
+};
 
 const respondJson = (req, res) => {
-  const result = estimator(inputData);
-  res.set('Content-Type',
-    'application/json');
-  res.send(result);
-}
+    const result = estimator(inputData);
+    res.set('Content-Type',
+        'application/json');
+    res.send(result);
+};
 
 const respondXml = (req, res) => {
-  const jsonResult = estimator(inputData);
-  const xmlResult = jsonxml(jsonResult);
-  res.set('Content-Type',
-    'application/xml');
-  res.send(xmlResult);
-}
+    const jsonResult = estimator(inputData);
+    const xmlResult = jsonxml(jsonResult);
+    res.set('Content-Type',
+        'application/xml');
+    res.send(xmlResult);
+};
 
 const respondLogs = (req, res) => {
-  const file = fs.createReadStream(`${__dirname}/api.log`,
-    { flags: 'r' });
-  res.set('Content-Type',
-    'text/plain')
-  file.pipe(res)
-}
+    const file = fs.createReadStream(`${__dirname}/api.log`,
+        { flags: 'r' });
+    res.set('Content-Type',
+        'text/plain');
+    file.pipe(res);
+};
 
 app.get('/api/v1/on-covid-19',
-  respondJson)
+    respondJson);
 app.get('/api/v1/on-covid-19/json',
-  respondJson)
+    respondJson);
 app.get('/api/v1/on-covid-19/xml',
-  respondXml)
+    respondXml);
 app.get('/api/v1/on-covid-19/logs',
-  respondLogs)
+    respondLogs);
 app.get('*',
-  (req, res) => {
-    res.write("Please use the api endpoints")
-  })
+    (req, res) => {
+        res.write("Please use the api endpoints");
+    });
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 
 app.listen(port,
-  () => console.log(`Listening on ${port}`))
+    () => console.log(`Listening on ${port}`));
