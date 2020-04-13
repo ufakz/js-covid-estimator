ba4ea5d31f211b31e2e7b08a940357bfd7629271
Farouq Umar <ufakz99@gmail.com>
2020-04-13 22:21:04 +0100

    Edited estimator.js 5

--- src/estimator.js
+++ src/estimator.js
@@ -1,16 +1,18 @@
 const computeData = (inputData) => {
-    //Gets the required variables from the inputData object
-    let { region, periodType, timeToElapse, population, currentlyInfected, totalHospitalBeds } = inputData;
+    // Gets the required variables from the inputData object
+    const {
+        region, periodType, timeToElapse, currentlyInfected, totalHospitalBeds,
+    } = inputData;
     let infectionsByRequestedTime;
     let factor;
     let days;
 
-    //Checks for the periodType to resolve computation
-    if (periodType === "days") {
+    // Checks for the periodType to resolve computation
+    if (periodType === 'days') {
         days = timeToElapse;
         factor = Math.trunc(days / 3);
         infectionsByRequestedTime = Math.trunc(currentlyInfected * (2 ** factor));
-    } else if (periodType === "weeks") {
+    } else if (periodType === 'weeks') {
         days = timeToElapse * 7;
         factor = Math.trunc(days / 3);
         infectionsByRequestedTime = Math.trunc(currentlyInfected * (2 ** factor));
@@ -20,22 +22,22 @@ const computeData = (inputData) => {
         infectionsByRequestedTime = Math.trunc(currentlyInfected * (2 ** factor));
     }
 
-    //Gets 15% of the infectionsByRequestedTime
+    // Gets 15% of the infectionsByRequestedTime
     const severeCasesByRequestedTime = Math.trunc(0.15 * infectionsByRequestedTime);
 
-    //Gets 35% of beds as available beds
+    // Gets 35% of beds as available beds
     const availBeds = Math.trunc(0.35 * totalHospitalBeds);
 
-    //Gets the available hospital beds by requested time based on the available beds and severe cases
+    // Gets the available hospital beds by requested time based on the available beds and severe cases
     const hospitalBedsByRequestedTime = Math.trunc(availBeds - severeCasesByRequestedTime);
 
-    //Gets 5% of the infections
+    // Gets 5% of the infections
     const casesForICUByRequestedTime = Math.trunc(0.05 * infectionsByRequestedTime);
 
-    //Gets 2% of the infections
+    // Gets 2% of the infections
     const casesForVentilatorsByRequestedTime = Math.trunc(0.02 * infectionsByRequestedTime);
 
-    //Gets the amount of dollars lost
+    // Gets the amount of dollars lost
     const dollarsInFlight = Math.trunc((infectionsByRequestedTime * region.avgDailyIncomePopulation * region.avgDailyIncomeInUSD) / days);
 
     return {
@@ -44,62 +46,68 @@ const computeData = (inputData) => {
         hospitalBedsByRequestedTime,
         casesForICUByRequestedTime,
         casesForVentilatorsByRequestedTime,
-        dollarsInFlight
-    }
-}
+        dollarsInFlight,
+    };
+};
 
-let calculateImpact = (inputData) => {
-    let currentlyInfected = Math.trunc(inputData.reportedCases * 10);
+const calculateImpact = (inputData) => {
+    const currentlyInfected = Math.trunc(inputData.reportedCases * 10);
     inputData.currentlyInfected = currentlyInfected;
-    let { infectionsByRequestedTime,
+    const {
+        infectionsByRequestedTime,
         severeCasesByRequestedTime,
         hospitalBedsByRequestedTime,
         casesForICUByRequestedTime,
         casesForVentilatorsByRequestedTime,
-        dollarsInFlight } = computeData(inputData);
+        dollarsInFlight,
+    } = computeData(inputData);
 
-    let impact = {
+    const impact = {
         currentlyInfected,
         infectionsByRequestedTime,
         severeCasesByRequestedTime,
         hospitalBedsByRequestedTime,
         casesForICUByRequestedTime,
         casesForVentilatorsByRequestedTime,
-        dollarsInFlight
-    }
-    return impact;
+        dollarsInFlight,
+    };
 
-}
+    return impact;
+};
 
 const calculateSevereImpact = (inputData) => {
-    let currentlyInfected = Math.trunc(inputData.reportedCases * 50);
+    const currentlyInfected = Math.trunc(inputData.reportedCases * 50);
     inputData.currentlyInfected = currentlyInfected;
-    let { infectionsByRequestedTime,
+    const {
+        infectionsByRequestedTime,
         severeCasesByRequestedTime,
         hospitalBedsByRequestedTime,
         casesForICUByRequestedTime,
         casesForVentilatorsByRequestedTime,
-        dollarsInFlight } = computeData(inputData);
+        dollarsInFlight,
+    } = computeData(inputData);
 
-    let severeImpact = {
+    const severeImpact = {
         currentlyInfected,
         infectionsByRequestedTime,
         severeCasesByRequestedTime,
         hospitalBedsByRequestedTime,
         casesForICUByRequestedTime,
         casesForVentilatorsByRequestedTime,
-        dollarsInFlight
-    }
+        dollarsInFlight,
+    };
+
     return severeImpact;
-}
+};
 
 const covid19ImpactEstimator = (inputData) => {
-    let result = {};
+    const result = {};
     result.data = inputData;
     result.estimate = {};
     result.estimate.impact = calculateImpact(inputData);
     result.estimate.severeImpact = calculateSevereImpact(inputData);
+
     return result;
-}
+};
 
 module.exports = covid19ImpactEstimator;
