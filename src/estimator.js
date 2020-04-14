68767421eb2549470b267316ba8189bb73e6360e
Farouq Umar <ufakz99@gmail.com>
2020-04-14 13:16:57 +0100

    Edited estimator.js 23

--- src/estimator.js
+++ src/estimator.js
@@ -59,21 +59,4 @@ const covid19ImpactEstimator = (inputData) => {
   return result;
 };
 
-const sampleData = {
-  region: {
-    name: 'Africa',
-    avgAge: 19.7,
-    avgDailyIncomeInUSD: 4,
-    avgDailyIncomePopulation: 0.73
-  },
-  periodType: 'days',
-  timeToElapse: 38,
-  reportedCases: 2747,
-  population: 92931687,
-  totalHospitalBeds: 678874
-}
-
-console.log(covid19ImpactEstimator(sampleData))
-
-
-//export default covid19ImpactEstimator;
+export default covid19ImpactEstimator;
