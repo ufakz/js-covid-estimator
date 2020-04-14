cedc28b7e9f17d27ac0d46f8828bf9b80d015c2e
Farouq Umar <ufakz99@gmail.com>
2020-04-14 13:06:44 +0100

    Edited estimator.js 21

--- src/estimator.js
+++ src/estimator.js
@@ -20,42 +20,40 @@ const covid19ImpactEstimator = (inputData) => {
 
   const result = {
     data: input,
-    estimate: {
-      impact: {
-        currentlyInfected,
-        infectionsByRequestedTime:
-          (Math.trunc(currentlyInfected * rfactor)),
-        severeCasesByRequestedTime:
-          (Math.trunc(0.15 * Math.trunc(currentlyInfected * rfactor))),
-        hospitalBedsByRequestedTime:
-          (Math.trunc((0.35 * input.totalHospitalBeds)
-            - Math.trunc(0.15 * Math.trunc(currentlyInfected * rfactor)))),
-        casesForICUByRequestedTime:
-          (Math.trunc(0.05 * currentlyInfected * rfactor)),
-        casesForVentilatorsByRequestedTime:
-          (Math.trunc(0.02 * currentlyInfected * rfactor)),
-        dollarsInFlight:
-          Math.trunc((currentlyInfected * rfactor
-            * input.region.avgDailyIncomePopulation * input.region.avgDailyIncomeInUSD)
-            / days)
-      },
-      severeImpact: {
-        currentlyInfected: severeCurrentlyInfected,
-        infectionsByRequestedTime:
-          (Math.trunc(severeCurrentlyInfected * rfactor)),
-        severeCasesByRequestedTime:
-          (Math.trunc(0.15 * Math.trunc(severeCurrentlyInfected * rfactor))),
-        hospitalBedsByRequestedTime:
-          (Math.trunc(0.35 * input.totalHospitalBeds)
-            - Math.trunc(0.15 * severeCurrentlyInfected * rfactor)),
-        casesForICUByRequestedTime:
-          (Math.trunc(0.05 * severeCurrentlyInfected * rfactor)),
-        casesForVentilatorsByRequestedTime:
-          (Math.trunc(0.02 * severeCurrentlyInfected * rfactor)),
-        dollarsInFlight: Math.trunc((severeCurrentlyInfected * rfactor
+    impact: {
+      currentlyInfected,
+      infectionsByRequestedTime:
+        (Math.trunc(currentlyInfected * rfactor)),
+      severeCasesByRequestedTime:
+        (Math.trunc(0.15 * Math.trunc(currentlyInfected * rfactor))),
+      hospitalBedsByRequestedTime:
+        (Math.trunc((0.35 * input.totalHospitalBeds)
+          - Math.trunc(0.15 * Math.trunc(currentlyInfected * rfactor)))),
+      casesForICUByRequestedTime:
+        (Math.trunc(0.05 * currentlyInfected * rfactor)),
+      casesForVentilatorsByRequestedTime:
+        (Math.trunc(0.02 * currentlyInfected * rfactor)),
+      dollarsInFlight:
+        Math.trunc((currentlyInfected * rfactor
           * input.region.avgDailyIncomePopulation * input.region.avgDailyIncomeInUSD)
           / days)
-      }
+    },
+    severeImpact: {
+      currentlyInfected: severeCurrentlyInfected,
+      infectionsByRequestedTime:
+        (Math.trunc(severeCurrentlyInfected * rfactor)),
+      severeCasesByRequestedTime:
+        (Math.trunc(0.15 * Math.trunc(severeCurrentlyInfected * rfactor))),
+      hospitalBedsByRequestedTime:
+        (Math.trunc(0.35 * input.totalHospitalBeds)
+          - Math.trunc(0.15 * severeCurrentlyInfected * rfactor)),
+      casesForICUByRequestedTime:
+        (Math.trunc(0.05 * severeCurrentlyInfected * rfactor)),
+      casesForVentilatorsByRequestedTime:
+        (Math.trunc(0.02 * severeCurrentlyInfected * rfactor)),
+      dollarsInFlight: Math.trunc((severeCurrentlyInfected * rfactor
+        * input.region.avgDailyIncomePopulation * input.region.avgDailyIncomeInUSD)
+        / days)
     }
   };
   return result;
